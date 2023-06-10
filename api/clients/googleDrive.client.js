d8605f2aa450b10558a9ad6b9f55cef271db6c61
Allakazan <brunorizzardi80014@hotmail.com>
2023-06-09 21:50:28 -0300

    feat: gdrive api

---
+++ api/clients/googleDrive.client.js
@@ -0,0 +1,22 @@
+
+const axios = require("axios");
+
+module.exports = class GoogleDriveService {
+    //propriedades e funções da classe aqui
+    constructor() {
+        this.url = 'https://www.googleapis.com/drive';
+        this.apiKey = 'AIzaSyAdUOqgDt0YF3of4QqSxZuWB8avIYR9J1k';
+    }
+
+    async getFile(id) {
+        return await axios.get(`${this.url}/v3/files/${id}`, { params: {
+            key: this.apiKey,
+            alt: 'media'
+        },
+        responseType: 'arraybuffer'});
+    }
+
+    async getFileDrive(id) {
+        return await axios.get(`https://drive.google.com/uc`, { params: { id }, responseType: 'arraybuffer'});
+    }
+}
--- api/file.js
+++ api/file.js
@@ -1,21 +1,42 @@
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
 
+const GoogleDriveService = require('./clients/googleDrive.client')
 /**
  * GET file list.
  *
  * @return file list | empty.
  */
 router.get("/", async (req, res) => {
-  try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
-  }
+    try {
+        res.json({
+            status: 200,
+            message: "Get data has successfully",
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+});
+
+
+router.get("/:fileId", async (req, res) => {
+    try {
+        const { data, status, headers } = await new GoogleDriveService()
+            .getFile(req.params.fileId);
+
+        res.set({
+            'Content-Type': headers['content-type'],
+            'Content-disposition': headers['content-disposition'],
+            'Content-Length': headers['content-length']
+        });
+        
+        res.send(data);
+        
+    } catch (error) {
+        return res.status(error.config.status).send(error.config.data);
+    }
 });
 
 module.exports = router;
